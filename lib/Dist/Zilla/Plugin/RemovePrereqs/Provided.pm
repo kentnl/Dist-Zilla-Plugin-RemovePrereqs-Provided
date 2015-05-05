db325993cdbc2d68e6a8632331639618d347e425
Kent Fredric <kentfredric@gmail.com>
2015-05-06 11:27:49 +1200

    Build results of 62efa95 (on master)

--- lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
+++ lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
@@ -6,14 +6,17 @@ package Dist::Zilla::Plugin::RemovePrereqs::Provided;
 
 our $VERSION = '0.001000';
 
-# ABSTRACT: Remove prereqs that are already provided.
+# ABSTRACT: Remove prerequisites that are already provided.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moose qw( has with );
+use Moose qw( has with around );
 use Moose::Util::TypeConstraints qw( enum );
+use Dist::Zilla::Util::ConfigDumper qw( config_dumper );
 
 my $sources = enum [qw( metaprovides )];
+
+no Moose::Util::TypeConstraints;
 
 with 'Dist::Zilla::Role::PrereqSource';
 
@@ -22,19 +25,22 @@ has provided_source => (
   is      => 'ro',
   default => sub { qw( metaprovides ) },
 );
+
+around dump_config => config_dumper( __PACKAGE__, { attrs => 'provided_source' } );
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
 sub _get_provides_metaprovides {
   my ( $self, ) = @_;
-  my (@plugins) = @{ $self->zilla->plugins_with('-MetaProvides::Provider') || [] };
+  my (@plugins) = @{ $self->zilla->plugins_with('-MetaProvider::Provider') || [] };
   if ( not @plugins ) {
-    $self->log("No MetaProvides::Provider plugins found in dist to extract metaprovides from");
+    $self->log('No MetaProvides::Provider plugins found in dist to extract metaprovides from');
     return ();
   }
   my @provided = map { $_->provides } @plugins;
   if ( not @plugins ) {
-    $self->log("No modules found while extracting provides from MetaProvider::Provider plugins");
+    $self->log('No modules found while extracting provides from MetaProvider::Provider plugins');
     return ();
   }
   return map { $_->module } @provided;
@@ -44,14 +50,15 @@ my @phases = qw(configure build test runtime develop);
 my @types  = qw(requires recommends suggests conflicts);
 
 sub register_prereqs {
-  my ( $self ) = @_;
-  my $prereqs = $self->zilla->prereqs;
-  my $method = "_get_provides_" . $self->provided_source;
+  my ($self)    = @_;
+  my $prereqs   = $self->zilla->prereqs;
+  my $method    = '_get_provides_' . $self->provided_source;
   my (@modules) = $self->$method;
-  for my $phase ( @phases ) {
-    for my $type ( @types ) {
-      for my $module ( @modules ) {
-        $prereqs->requirements_for( $phase, $type )->clear_requirements($module);
+  for my $phase (@phases) {
+    for my $type (@types) {
+      my $reqs = $prereqs->requirements_for( $phase, $type );
+      for my $module (@modules) {
+        $reqs->clear_requirement($module);
       }
     }
   }
@@ -68,7 +75,7 @@ __END__
 
 =head1 NAME
 
-Dist::Zilla::Plugin::RemovePrereqs::Provided - Remove prereqs that are already provided.
+Dist::Zilla::Plugin::RemovePrereqs::Provided - Remove prerequisites that are already provided.
 
 =head1 VERSION
 
@@ -76,8 +83,12 @@ version 0.001000
 
 =head1 DESCRIPTION
 
-This is an initial implementation that assumes you have L<< C<[MetaProvides]>|Dist::Zilla::Plugin::MetaProvides >> of some description in place, and
-uses the data it provides to make sure the same modules don't exist as prerequisites.
+This module is a utility for people who are working with self-consuming code ( predominantly C<Dist::Zilla> distributions )
+who wish to avoid self-dependencies in cases where some other prerequisite providing tool is over-zealous in determining
+prerequisites.
+
+This is an initial implementation that assumes you have L<< C<[MetaProvides]>|Dist::Zilla::Plugin::MetaProvides >> of some
+description in place, and uses the data it provides to make sure the same modules don't exist as prerequisites.
 
 =head1 AUTHOR
 
