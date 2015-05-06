0a605308597234ae22e35fc4b94eedb982e9724d
Kent Fredric <kentfredric@gmail.com>
2015-05-06 14:45:12 +1200

    Build results of 70c1d4b (on master)

--- lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
+++ lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::RemovePrereqs::Provided;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Remove prerequisites that are already provided.
 
@@ -83,7 +83,7 @@ Dist::Zilla::Plugin::RemovePrereqs::Provided - Remove prerequisites that are alr
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
