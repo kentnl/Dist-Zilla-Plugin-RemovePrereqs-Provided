3037d7410852af65f8c2a1f019e1bc890844438a
Kent Fredric <kentfredric@gmail.com>
2017-03-03 23:17:34 +1300

    Build results of 0c4ffa5 (on master)

--- lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
+++ lib/Dist/Zilla/Plugin/RemovePrereqs/Provided.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::RemovePrereqs::Provided;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Remove prerequisites that are already provided.
 
@@ -83,7 +83,7 @@ Dist::Zilla::Plugin::RemovePrereqs::Provided - Remove prerequisites that are alr
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
@@ -102,7 +102,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
